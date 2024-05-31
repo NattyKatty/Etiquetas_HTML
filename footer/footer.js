5600665a1e50dd44939365b669e9c77c33fed5aa
Natty <ndmhernandez@edu.gt>
2024-05-30 18:57:07 -0600

    Añadido Header y Footer.

--- footer/footer.js
+++ footer/footer.js
@@ -1,3 +1,18 @@
 let footer_DOM = document.querySelector(".footer")
 
-footer_DOM.innerHTML = 'HIII, Soy Footer!'
+footer_DOM.innerHTML = 'Hola! Soy Footer!'
+
+document.addEventListener('DOMContentLoaded', function() {
+    const footerElement = document.querySelector('.footer');
+    footerElement.innerHTML = `
+        <img src="https://static.wixstatic.com/media/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png/v1/fit/w_2500,h_1330,al_c/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png" alt="Collaborator Image">
+        <div class="colaboradoras">
+            <p>Colaboradoras:</p>
+            <ul>
+                <li>Vane - Colaboradora 1</li>
+                <li>Miranda - Colaboradora 2</li>
+                <li>Natty - Colaboradora 3</li>
+            </ul>
+        </div>
+    `;
+});
--- header/header.js
+++ header/header.js
@@ -1,3 +1,11 @@
 let header_DOM = document.querySelector(".header")
 
-header_DOM.innerHTML = 'HIII, Soy Header!'
+header_DOM.innerHTML = 'Hola! Soy Header.'
+
+document.addEventListener('DOMContentLoaded', function() {
+    const headerElement = document.querySelector('.header');
+    headerElement.innerHTML = `
+        <h1>Gramática HTML</h1>
+        <a class="github-button" href="https://github.com/NattyKatty/Etiquetas_HTML" target="_blank">GitHub</a>
+    `;
+});
